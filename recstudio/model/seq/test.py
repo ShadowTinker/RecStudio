fd00a9c575174e9dd63eb1e0b9cb1cfb18de0070
ShadowTinker <shadowtinkery@gmail.com>
2023-05-08 22:38:50 +0800

    [feat] add test model and change some NNI configs

---
+++ recstudio/model/seq/test.py
@@ -0,0 +1,16 @@
+import torch
+from recstudio.data import dataset
+from recstudio.model.module import data_augmentation
+from recstudio.model.seq.sasrec import SASRec, SASRecQueryEncoder
+
+class Test(SASRec):
+    def _init_model(self, train_data):
+        super()._init_model(train_data)
+        self.augmentation_model = data_augmentation.MyAugmentation(self.config['model'], train_data)
+
+    def training_step(self, batch):
+        output = self.forward(batch, False)
+        cl_output = self.augmentation_model(batch, self.item_encoder)
+        loss_value = self.loss_fn(batch[self.frating], **output['score']) \
+           + self.config['model']['cl_weight'] * cl_output['cl_loss']
+        return loss_value
