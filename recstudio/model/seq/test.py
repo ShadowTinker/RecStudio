78b16eaa01907c1c1542bb8537261193dc070391
ShadowTinker <shadowtinkery@gmail.com>
2023-05-10 16:09:33 +0800

    [feat] change NNI config

--- recstudio/model/seq/test.py
+++ recstudio/model/seq/test.py
@@ -2,6 +2,7 @@ import torch
 from recstudio.data import dataset
 from recstudio.model.module import data_augmentation
 from recstudio.model.seq.sasrec import SASRec, SASRecQueryEncoder
+from recstudio.model import loss_func
 
 class Test(SASRec):
     def _init_model(self, train_data):
@@ -9,8 +10,14 @@ class Test(SASRec):
         self.augmentation_model = data_augmentation.MyAugmentation(self.config['model'], train_data)
 
     def training_step(self, batch):
-        output = self.forward(batch, False)
+        output = self.forward(batch, isinstance(self.loss_fn, loss_func.FullScoreLoss))
         cl_output = self.augmentation_model(batch, self.item_encoder.weight)
         loss_value = self.loss_fn(batch[self.frating], **output['score']) \
            + self.config['model']['cl_weight'] * cl_output['cl_loss']
         return loss_value
+
+    def _get_loss_func(self):
+        return loss_func.SoftmaxLoss()
+    
+    def _get_sampler(self, train_data):
+        return None
