9adc1b0730ba76235627f8dac6befeafebcb1902
ShadowTinker <shadowtinkery@gmail.com>
2023-08-31 19:53:57 +0800

    [feat] SASRec_CDR now gets query encoder in SASRec

--- recstudio/model/cdr/sasrec_cdr.py
+++ recstudio/model/cdr/sasrec_cdr.py
@@ -4,68 +4,7 @@ from recstudio.data import dataset
 from recstudio.model.module import functional as recfn
 from recstudio.model import basemodel, loss_func, module, scorer
 import torch.nn.functional as F
-
-class SASRecQueryEncoder(torch.nn.Module):
-    def __init__(
-            self, fiid, embed_dim, max_seq_len, n_head, hidden_size, dropout, activation, layer_norm_eps, n_layer, item_encoder,
-            bidirectional=False, training_pooling_type='last', eval_pooling_type='last') -> None:
-        super().__init__()
-        self.fiid = fiid
-        self.item_encoder = item_encoder
-        self.bidirectional = bidirectional
-        self.training_pooling_type = training_pooling_type
-        self.eval_pooling_type = eval_pooling_type
-        self.position_emb = torch.nn.Embedding(max_seq_len, embed_dim)
-        transformer_encoder = torch.nn.TransformerEncoderLayer(
-            d_model=embed_dim,
-            nhead=n_head,
-            dim_feedforward=hidden_size,
-            dropout=dropout,
-            activation=activation,
-            layer_norm_eps=layer_norm_eps,
-            batch_first=True,
-            norm_first=False
-        )
-        self.transformer_layer = torch.nn.TransformerEncoder(
-            encoder_layer=transformer_encoder,
-            num_layers=n_layer,
-        )
-        self.dropout = torch.nn.Dropout(p=dropout)
-        self.training_pooling_layer = module.SeqPoolingLayer(pooling_type=self.training_pooling_type)
-        self.eval_pooling_layer = module.SeqPoolingLayer(pooling_type=self.eval_pooling_type)
-
-    def forward(self, batch, need_pooling=True):
-        user_hist = batch['in_'+self.fiid]
-        positions = torch.arange(user_hist.size(1), dtype=torch.long, device=user_hist.device)
-        positions = positions.unsqueeze(0).expand_as(user_hist)
-        position_embs = self.position_emb(positions)
-        seq_embs = self.item_encoder(user_hist)
-
-        mask4padding = user_hist == 0  # BxL
-        L = user_hist.size(-1)
-        if not self.bidirectional:
-            attention_mask = torch.triu(torch.ones((L, L), dtype=torch.bool, device=user_hist.device), 1)
-        else:
-            attention_mask = torch.zeros((L, L), dtype=torch.bool, device=user_hist.device)
-        transformer_out = self.transformer_layer(
-            src=self.dropout(seq_embs+position_embs),
-            mask=attention_mask,
-            src_key_padding_mask=mask4padding)  # BxLxD
-
-        if need_pooling:
-            if self.training:
-                if self.training_pooling_type == 'mask':
-                    return self.training_pooling_layer(transformer_out, batch['seqlen'], mask_token=batch['mask_token'])
-                else:
-                    return self.training_pooling_layer(transformer_out, batch['seqlen'])
-            else:
-                if self.eval_pooling_type == 'mask':
-                    return self.eval_pooling_layer(transformer_out, batch['seqlen'], mask_token=batch['mask_token'])
-                else:
-                    return self.eval_pooling_layer(transformer_out, batch['seqlen'])
-        else:
-            return transformer_out
-
+from recstudio.model.seq.sasrec import SASRecQueryEncoder
 
 class SASRec_CDR(basemodel.CrossRetriever):
     r"""
