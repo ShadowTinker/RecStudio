6155c7b4246b099a3e8fe653dc31280ec4f8ee81
ShadowTinker <shadowtinkery@gmail.com>
2023-09-01 17:09:44 +0800

    [feat] add different training objective for SASRec

--- recstudio/model/cdr/sasrec_cdr.py
+++ recstudio/model/cdr/sasrec_cdr.py
@@ -1,5 +1,7 @@
+from typing import Dict
 import torch
 from recstudio.ann import sampler
+from recstudio.ann.sampler import Dict
 from recstudio.data import dataset
 from recstudio.model.module import functional as recfn
 from recstudio.model import basemodel, loss_func, module, scorer
@@ -21,16 +23,7 @@ class SASRec_CDR(basemodel.CrossRetriever):
         - ``layer_norm_eps``: The layer norm epsilon in transformer. Default: ``1e-12``.
     """
 
-    # def add_model_specific_args(parent_parser):
-    #     parent_parser = basemodel.Recommender.add_model_specific_args(parent_parser)
-    #     parent_parser.add_argument_group('SASRec')
-    #     parent_parser.add_argument("--hidden_size", type=int, default=128, help='hidden size of feedforward')
-    #     parent_parser.add_argument("--layer_num", type=int, default=2, help='layer num of transformers')
-    #     parent_parser.add_argument("--head_num", type=int, default=2, help='head num of multi-head attention')
-    #     parent_parser.add_argument("--dropout_rate", type=float, default=0.5, help='dropout rate')
-    #     parent_parser.add_argument("--negative_count", type=int, default=1, help='negative sampling numbers')
-    #     return parent_parser
-
+    @staticmethod
     def _get_dataset_class():
         r"""SeqDataset is used for SASRec."""
         return dataset.CrossDomainSeqToSeqDataset
@@ -68,38 +61,44 @@ class SASRec_CDR(basemodel.CrossRetriever):
         x = F.normalize(x, dim=-1)
         return torch.pdist(x, p=2).pow(2).mul(-2).exp().mean().log()
 
-    # def training_step(self, batch):
-    #     outputs = self.forward(batch, isinstance(self.loss_fn, loss_func.FullScoreLoss), True, True)
-    #     loss_value = 0
-    #     for idx, domain in enumerate(self.SOURCE_DOMAINS):
-    #         domain_user, domain_item = outputs[domain]['query'], outputs[domain]['item']
-    #         domain_align_loss = self.alignment(domain_user, domain_item)
-    #         domain_uniformity_loss = self.uniformity(domain_user) + self.uniformity(domain_item)
-    #         domain_loss = domain_align_loss + self.config['model']['gamma'] * domain_uniformity_loss
-    #         loss_value += domain_loss
-    #     return loss_value
-
     def training_step(self, batch):
         outputs = self.forward(batch, isinstance(self.loss_fn, loss_func.FullScoreLoss), True, True)
         if 'score' in outputs:
             # domain sampling is set to 'togather'
             if self.config['model']['directau_loss']:
-                alignment = self.alignment(outputs['query'], outputs['item'])
-                mask = (batch['item_id'] != 0).flatten()
-                uniformity = (
-                    self.uniformity(outputs['query'].flatten(0, 1)[mask]) +
-                    self.uniformity(outputs['item'].flatten(0, 1)[mask])
-                )
-                loss_value = alignment + self.config['model']['gamma'] * uniformity
+                if self._get_dataset_class() is dataset.CrossDomainSeqToSeqDataset:
+                    alignment = self.alignment(outputs['query'], outputs['item'])
+                    mask = (batch['item_id'] != 0).flatten()
+                    uniformity = (
+                        self.uniformity(outputs['query'].flatten(0, 1)[mask]) +
+                        self.uniformity(outputs['item'].flatten(0, 1)[mask])
+                    )
+                    loss_value = alignment + self.config['model']['gamma'] * uniformity
+                elif self._get_dataset_class() is dataset.CrossDomainSeqDataset:
+                    alignment = self.alignment(outputs['query'].squeeze(), outputs['item'])
+                    uniformity = (
+                        self.uniformity(outputs['query'].squeeze()) +
+                        self.uniformity(outputs['item'])
+                    )
+                    loss_value = alignment + self.config['model']['gamma'] * uniformity
             else:
                 loss_value = self.loss_fn(batch[self.frating], **outputs['score'])
-
         else:
             loss_value = 0
-            for idx, domain in enumerate(self.SOURCE_DOMAINS):
-                if outputs.get(domain, None) == None:
-                    continue
-                loss_value += self.loss_fn(batch[domain][self.frating], **outputs[domain]['score'])
+            if self.config['model']['directau_loss']:
+                for idx, domain in enumerate(self.SOURCE_DOMAINS):
+                    if outputs.get(domain, None) == None:
+                        continue
+                    domain_user, domain_item = outputs[domain]['query'], outputs[domain]['item']
+                    domain_align_loss = self.alignment(domain_user, domain_item)
+                    domain_uniformity_loss = self.uniformity(domain_user) + self.uniformity(domain_item)
+                    domain_loss = domain_align_loss + self.config['model']['gamma'] * domain_uniformity_loss
+                    loss_value += domain_loss
+            else:
+                for idx, domain in enumerate(self.SOURCE_DOMAINS):
+                    if outputs.get(domain, None) == None:
+                        continue
+                    loss_value += self.loss_fn(batch[domain][self.frating], **outputs[domain]['score'])
         return loss_value
 
     def _get_sampler(self, meta_dataset):
